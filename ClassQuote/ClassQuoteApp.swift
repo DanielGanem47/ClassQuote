55895173364ae4ffe9344a0356dccec9f8a2634d
Daniel Ganem <daniel.ganem@icloud.com>
2025-06-17 16:36:37 +0200

    Initial Commit

---
+++ ClassQuote/ClassQuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassQuoteApp.swift
+//  ClassQuote
+//
+//  Created by daniel ganem on 17/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct ClassQuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClassQuoteTests/ClassQuoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClassQuoteTests.swift
+//  ClassQuoteTests
+//
+//  Created by daniel ganem on 17/06/2025.
+//
+
+import Testing
+@testable import ClassQuote
+
+struct ClassQuoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
