cae20781e1ba7f6fee3ca410192e8609cb5e072d
Daniel Ganem <daniel.ganem@icloud.com>
2025-06-18 18:17:00 +0200

    MAJ arbo

---
+++ ClassQuote/Services/QuoteService.swift
@@ -0,0 +1,43 @@
+//
+//  QuoteService.swift
+//  ClassQuote
+//
+//  Created by daniel ganem on 17/06/2025.
+//
+
+import Foundation
+import SwiftUICore
+
+final class QuoteService {
+    private let baseURL: URL = URL.init(filePath: "http://api.forismatic.com/api/1.0/")!
+    
+    var citation: String = ""
+    var author: String = ""
+    
+    func getQuote() {
+        var request: URLRequest = URLRequest(url: baseURL)
+        request.httpMethod = "POST"
+        
+        let body: String = "method=getQuote&format=json@lang=en"
+        request.httpBody = body.data(using: .utf8)
+        
+        let session: URLSession = URLSession(configuration: .default)
+        let task = session.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                return
+            }
+            let decode: JSONDecoder = JSONDecoder()
+            guard let responseJSON = try? decode.decode([String : String].self, from: data),
+                    let text = responseJSON["quoteText"],
+                    let auth = responseJSON["quoteAuthor"] else {
+                return
+            }
+            self.citation = text
+            self.author = auth
+        }
+        task.resume()
+    }
+}
